446eb24e4c196959d6bf366aa9ed791f4890fe67
Pablo Emannoel <165969695+tawazu23@users.noreply.github.com>
2025-06-07 10:44:46 -0300

    Add files via upload

---
+++ mundo4pratica5/generator.py
@@ -0,0 +1,76 @@
+import asyncio
+import random
+import uuid
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+#   Run 'pip install azure-iot-device' to install the required libraries for this application
+#   Note: Requires Python 3.6+ 
+
+from azure.iot.device.aio import IoTHubDeviceClient
+from azure.iot.device import Message
+
+# The device connection string to authenticate the device with your IoT hub.
+CONNECTION_STRING = os.getenv("CONNECTION_STRING")
+
+MESSAGE_TIMEOUT = 10000
+
+# Define the JSON message to send to IoT Hub.
+TEMPERATURE = 20.0
+HUMIDITY = 60
+MSG_TXT = "{\"temperature\": %.2f,\"humidity\": %.2f}"
+
+# Temperature threshold for alerting
+TEMP_ALERT_THRESHOLD = 30
+
+async def main():
+    try:
+        client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+        await client.connect()
+
+        print("IoT Hub device sending periodic messages, press Ctrl-C to exit")
+
+        while True:
+            # Build the message with simulated telemetry values.
+            temperature = TEMPERATURE + (random.random() * 15)
+            humidity = HUMIDITY + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT % (temperature, humidity)
+            message = Message(msg_txt_formatted)
+
+            # Add standard message properties
+            message.message_id = uuid.uuid4()
+            message.content_encoding = "utf-8"
+            message.content_type = "application/json"
+
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            prop_map = message.custom_properties
+            prop_map["temperatureAlert"] = ("true" if temperature > TEMP_ALERT_THRESHOLD else "false")
+
+            # Send the message.
+            print("Sending message: %s" % message.data)
+            try:
+                await client.send_message(message)
+            except Exception as ex:
+                print("Error sending message from device: {}".format(ex))
+            await asyncio.sleep(1)
+
+    except Exception as iothub_error:
+        print("Unexpected error %s from IoTHub" % iothub_error)
+        return
+    except asyncio.CancelledError:
+        await client.shutdown()
+        print('Shutting down device client')
+
+if __name__ == '__main__':
+    print("IoT Hub simulated device")
+    print("Press Ctrl-C to exit")
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Keyboard Interrupt - sample stopped')
